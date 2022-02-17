d1d97b6fd34636d04fbbc06118dd527817ee950b
mananshah228 <98092667+mananshah228@users.noreply.github.com>
2022-02-17 23:27:14 +0530

    Update Binary_Search_Tree.c

--- Binary Search Tree/Binary_Search_Tree.c
+++ Binary Search Tree/Binary_Search_Tree.c
@@ -1,114 +1,333 @@
-#include <iostream>  
-using namespace std;  
-struct Node {  
-    int data;  
-    Node *left;  
-    Node *right;  
-};  
-Node* create(int item)  
-{  
-    Node* node = new Node;  
-    node->data = item;  
-    node->left = node->right = NULL;  
-    return node;  
-}  
-/*Inorder traversal of the tree formed*/  
-void inorder(Node *root)  
-{  
-    if (root == NULL)  
-        return;  
-    inorder(root->left); //traverse left subtree  
-    cout<< root->data << "   "; //traverse root node  
-    inorder(root->right); //traverse right subtree  
-}  
-Node* findMinimum(Node* cur) /*To find the inorder successor*/  
-{  
-    while(cur->left != NULL) {  
-        cur = cur->left;  
-    }  
-    return cur;  
-}  
-Node* insertion(Node* root, int item) /*Insert a node*/  
-{  
-    if (root == NULL)  
-        return create(item); /*return new node if tree is empty*/  
-    if (item < root->data)  
-        root->left = insertion(root->left, item);  
-    else  
-        root->right = insertion(root->right, item);  
-    return root;  
-}  
-void search(Node* &cur, int item, Node* &parent)  
-{  
-    while (cur != NULL && cur->data != item)  
-    {  
-        parent = cur;  
-        if (item < cur->data)  
-            cur = cur->left;  
-        else  
-            cur = cur->right;  
-    }  
-}  
-void deletion(Node*& root, int item) /*function to delete a node*/  
-{  
-    Node* parent = NULL;  
-    Node* cur = root;  
-    search(cur, item, parent); /*find the node to be deleted*/  
-    if (cur == NULL)  
-        return;  
-    if (cur->left == NULL && cur->right == NULL) /*When node has no children*/  
-    {  
-        if (cur != root)  
-        {  
-            if (parent->left == cur)  
-                parent->left = NULL;  
-            else  
-                parent->right = NULL;  
-        }  
-        else  
-            root = NULL;  
-        free(cur);       
-    }  
-    else if (cur->left && cur->right)  
-    {  
-        Node* succ  = findMinimum(cur->right);  
-        int val = succ->data;  
-        deletion(root, succ->data);  
-        cur->data = val;  
-    }  
-    else  
-    {  
-        Node* child = (cur->left)? cur->left: cur->right;  
-        if (cur != root)  
-        {  
-            if (cur == parent->left)  
-                parent->left = child;  
-            else  
-                parent->right = child;  
-        }  
-        else  
-            root = child;  
-        free(cur);  
-    }  
-}  
-int main()  
-{  
-  Node* root = NULL;  
-  root = insertion(root, 45);  
-  root = insertion(root, 30);  
-  root = insertion(root, 50);  
-  root = insertion(root, 25);  
-  root = insertion(root, 35);  
-  root = insertion(root, 45);  
-  root = insertion(root, 60);  
-  root = insertion(root, 4);  
-  printf("The inorder traversal of the given binary tree is - \n");  
-  inorder(root);  
-  deletion(root, 25);  
-  printf("\nAfter deleting node 25, the inorder traversal of the given binary tree is - \n");  
-  inorder(root);   
-  insertion(root, 2);  
-  printf("\nAfter inserting node 2, the inorder traversal of the given binary tree is - \n");  
-  inorder(root);  
-  return 0;  
-}  
+#include <stdio.h>
+
+#include <stdlib.h>
+
+struct BST
+
+{
+
+int data;
+
+struct BST *left;
+
+struct BST *right;
+
+};
+
+typedef struct BST NODE;
+
+NODE *node;
+
+NODE* createtree(NODE *node, int data)
+
+{
+
+if (node == NULL)
+
+{
+
+NODE *temp;
+
+temp= (NODE*)malloc(sizeof(NODE));
+
+temp->data = data;
+
+temp->left = temp->right = NULL;
+
+return temp;
+
+}
+
+if (data < (node->data))
+
+{
+
+node->left = createtree(node->left, data);
+
+}
+
+else if (data > node->data)
+
+{
+
+node -> right = createtree(node->right, data);
+
+}
+
+return node;
+
+}
+
+NODE* search(NODE *node, int data)
+
+{
+
+if(node == NULL)
+
+printf("\nElement not found");
+
+else if(data < node->data)
+
+{
+
+node->left=search(node->left, data);
+
+}
+
+else if(data > node->data)
+
+{
+
+node->right=search(node->right, data);
+
+}
+
+else
+
+printf("\nElement found is: %d", node->data);
+
+return node;
+
+}
+
+void inorder(NODE *node)
+
+{
+
+if(node != NULL)
+
+{
+
+inorder(node->left);
+
+printf("%d\t", node->data);
+
+inorder(node->right);
+
+}
+
+}
+
+void preorder(NODE *node)
+
+{
+
+if(node != NULL)
+
+{
+
+printf("%d\t", node->data);
+
+preorder(node->left);
+
+preorder(node->right);
+
+}
+
+}
+
+void postorder(NODE *node)
+
+{
+
+if(node != NULL)
+
+{
+
+postorder(node->left);
+
+postorder(node->right);
+
+printf("%d\t", node->data);
+
+}
+
+}
+
+NODE* findMin(NODE *node)
+
+{
+
+if(node==NULL)
+
+{
+
+return NULL;
+
+}
+
+if(node->left)
+
+return findMin(node->left);
+
+else
+
+return node;
+
+}
+
+NODE* del(NODE *node, int data)
+
+{
+
+NODE *temp;
+
+if(node == NULL)
+
+{
+
+printf("\nElement not found");
+
+}
+
+else if(data < node->data)
+
+{
+
+node->left = del(node->left, data);
+
+}
+
+else if(data > node->data)
+
+{
+
+node->right = del(node->right, data);
+
+}
+
+else
+
+{ /* Now We can delete this node and replace with either minimum element in the right sub tree or maximum element in the left subtree */
+
+if(node->right && node->left)
+
+{ /* Here we will replace with minimum element in the right sub tree */
+
+temp = findMin(node->right);
+
+node -> data = temp->data;
+
+/* As we replaced it with some other node, we have to delete that node */
+
+node -> right = del(node->right,temp->data);
+
+}
+
+else
+
+{
+
+/* If there is only one or zero children then we can directly remove it from the tree and connect its parent to its child */
+
+temp = node;
+
+if(node->left == NULL)
+
+node = node->right;
+
+else if(node->right == NULL)
+
+node = node->left;
+
+free(temp); /* temp is longer required */
+
+}
+
+}
+
+return node;
+
+}
+
+void main()
+
+{
+
+int data, ch, i, n;
+
+NODE *root=NULL;
+
+while (1)
+
+{
+
+printf("\n1.Insertion in Binary Search Tree");
+
+printf("\n2.Search Element in Binary Search Tree");
+
+printf("\n3.Delete Element in Binary Search Tree");
+
+printf("\n4.Inorder\n5.Preorder\n6.Postorder\n7.Exit");
+
+printf("\nEnter your choice: ");
+
+scanf("%d", &ch);
+
+switch (ch)
+
+{
+
+case 1: printf("\nEnter N value: " );
+
+scanf("%d", &n);
+
+printf("\nEnter the values to create BST like(6,9,5,2,8,15,24,14,7,8,5,2)\n");
+
+for(i=0; i<n; i++)
+
+{
+
+scanf("%d", &data);
+
+root=createtree(root, data);
+
+}
+
+break;
+
+case 2: printf("\nEnter the element to search: ");
+
+scanf("%d", &data);
+
+root=search(root, data);
+
+break;
+
+case 3: printf("\nEnter the element to delete: ");
+
+scanf("%d", &data);
+
+root=del(root, data);
+
+break;
+
+case 4: printf("\nInorder Traversal: \n");
+
+inorder(root);
+
+break;
+
+case 5: printf("\nPreorder Traversal: \n");
+
+preorder(root);
+
+break;
+
+case 6: printf("\nPostorder Traversal: \n");
+
+postorder(root);
+
+break;
+
+case 7: exit(0);
+
+default:printf("\nWrong option");
+
+break;
+
+}
+
+}
+
+}
